a36c3d34c585ba0506997254844421fdf2b5fae0
Jean Senellart <jean.senellart@quandela.com>
2022-04-11 16:46:22 +0200

    #PCVL-76: fix call to permanent with named parameter

--- perceval/backends/naive.py
+++ perceval/backends/naive.py
@@ -1,3 +1,25 @@
+# MIT License
+#
+# Copyright (c) 2022 Quandela
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 import math
 import numpy as np
 
@@ -31,7 +53,7 @@ class NaiveBackend(Backend):
                         Ust[rowidx, colidx] = self._U[ok, ik]
                         rowidx += 1
                 colidx += 1
-        return qc.permanent_cx(Ust, 1)/math.sqrt(p)
+        return qc.permanent_cx(Ust, n_threads=1)/math.sqrt(p)
 
     def prob_be(self, input_state, output_state, n=None, output_idx=None):
         return abs(self.probampli_be(input_state, output_state, n, output_idx))**2
