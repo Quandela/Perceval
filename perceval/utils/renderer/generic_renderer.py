d998238e5df683cb9a05a9fb3bea1b354e1f3057
Jean Senellart <jean.senellart@quandela.com>
2022-05-16 11:13:56 +0200

    fix bug with renderer

--- perceval/utils/renderer/generic_renderer.py
+++ perceval/utils/renderer/generic_renderer.py
@@ -194,7 +194,7 @@ class Canvas(ABC):
                 norm_points += ["L", self.position[0], self.position[1]]
                 points = points[2:]
             elif points[0] == "h":
-                self.position = (points[1]+self.relative_position[0], self.position[1])
+                self.position = (points[1]+self.relative_position[0], self.relative_position[1])
                 norm_points += ["L", self.position[0], self.position[1]]
                 points = points[2:]
             elif points[0] == "V":
@@ -202,7 +202,7 @@ class Canvas(ABC):
                 norm_points += ["L", self.position[0], self.position[1]]
                 points = points[2:]
             elif points[0] == "v":
-                self.position = (self.position[0], points[1]+self.relative_position[1])
+                self.position = (self.relative_position[0], points[1]+self.relative_position[1])
                 norm_points += ["L", self.position[0], self.position[1]]
                 points = points[2:]
             else:
