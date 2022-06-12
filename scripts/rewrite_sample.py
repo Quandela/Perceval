8c73d5d4db639c6f3284359ee39479d68f48b4bf
Jean Senellart <jean.senellart@quandela.com>
2022-06-12 22:48:25 +0200

    implement interferometer reflexivity correction

--- scripts/rewrite_sample.py
+++ scripts/rewrite_sample.py
@@ -23,25 +23,22 @@
 import perceval as pcvl
 import perceval.lib.phys as phys
 from perceval.algorithm.optimize import optimize
-from perceval.algorithm import frobenius
+from perceval.algorithm.norm import fidelity, frobenius
 import time
 import random
 
-pattern1=pcvl.Circuit(3, name="pattern1")//(0,phys.PS(pcvl.P("phi0")))//(0,phys.BS(theta=pcvl.P("theta1")))//(0,phys.PS(pcvl.P("phi2")))//(1,phys.PS(pcvl.P("phi1")))//(1,phys.BS(theta=pcvl.P("theta2")))//(0,phys.BS(theta=pcvl.P("theta3")))
-pattern1._color = "lightgreen"
-pcvl.pdisplay(pcvl.Circuit(3).add(0,pattern1,False), recursive=True)
-
-rewrite1=pcvl.Circuit(3, name="rewrite1")//(0,phys.PS(pcvl.P("beta2")))//(1,phys.PS(pcvl.P("beta1")))//(1,phys.BS(theta=pcvl.P("alpha1")))//(0,phys.BS(theta=pcvl.P("alpha2")))//(1,phys.PS(pcvl.P("beta3")))//(1,phys.BS(theta=pcvl.P("alpha3")))//(0,phys.PS(pcvl.P("beta4")))//(1,phys.PS(pcvl.P("beta5")))//(2,phys.PS(pcvl.P("beta6")))
-rewrite1._color = "pink"
-pcvl.pdisplay(pcvl.Circuit(3).add(0,rewrite1,False), recursive=True)
+pattern1=pcvl.Circuit(2, name="pattern1")//(0,phys.PS(pcvl.P("phi1")))//(0,phys.BS(R=0.5))//(0,phys.PS(pcvl.P("phi2")))//(0,phys.BS(R=0.5))
+rewrite1=pcvl.Circuit(2, name="rewrite1")//(0,phys.PS(pcvl.P("phi1")))//(0,phys.BS(R=0.42))//(0,phys.PS(pcvl.P("phi2")))//(0,phys.BS(R=0.42))//(0,phys.PS(pcvl.P("phi3")))//(1,phys.PS(pcvl.P("phi4")))
 
 pattern2=pcvl.Circuit(1, name="pattern2")//phys.PS(pcvl.P("phi1"))//phys.PS(pcvl.P("phi2"))
 rewrite2=pcvl.Circuit(1, name="rewrite2")//phys.PS(pcvl.P("phi"))
 
-a=pcvl.Circuit.generic_interferometer(8,
-                                      lambda idx:pcvl.Circuit(2)//phys.PS(phi=random.random())//phys.BS(theta=random.random()),
-                                      shape="rectangle")
-#pcvl.pdisplay(a, recursive=True, render_size=1)
+pattern3=pcvl.Circuit(2, name="pattern3")//(1,phys.PS(pcvl.P("phip")))//(0,phys.BS(R=0.42))
+rewrite3=pcvl.Circuit(2, name="rewrite3")//(0,phys.PS(pcvl.P("phi1")))//(0,phys.BS(R=0.42))//(0,phys.PS(pcvl.P("phi2")))//(1,phys.PS(pcvl.P("phi3")))
+
+a = pcvl.Circuit.generic_interferometer(8,
+                                        lambda idx:pcvl.Circuit(2)//phys.PS(phi=random.random())//phys.BS(R=0.5)//phys.PS(phi=random.random())//phys.BS(R=0.5), shape="rectangle")
+u = a.compute_unitary(False)
 
 current = time.time()
 def tick(description):
@@ -50,33 +47,28 @@ def tick(description):
     print("%f\t%s" % (dt, description))
     current = time.time()
 
-rules = [(pattern1, rewrite1, "lightgreen"), (pattern2, rewrite2, "pink")]
+rules = [(pattern1, rewrite1, "lightgreen"), (pattern2, rewrite2, "pink"), (pattern3, rewrite3, "lightgray")]
+
 while True:
-    found = False
+    found_match = False
     for pattern, rewrite, color in rules:
         start_pos = 0
-        while True:
-            matched = a.match(pattern, browse=True, pos=start_pos)
-            tick("match: %s - %s" % (pattern._name, matched and "ok" or "nok"))
-            if matched is None:
-                break
-            idx = a.isolate(list(matched.pos_map.keys()), color=color)
-            tick("└─ isolate match")
-            for k, v in matched.v_map.items():
-                pattern[k].set_value(v)
-            tick("└─ set values")
-            v = pattern.compute_unitary(False)
-            tick("└─ compute unitary")
-            res = optimize(rewrite, v, frobenius, sign=-1)
-            tick("└─ optimize %s - %f" % (rewrite._name, res.fun))
-            subc = rewrite.copy()
-            tick("└─ rewrite")
-            found = True
-            a.replace(idx, subc, merge=True)
-            tick("└─ replace")
-            pattern.reset_parameters()
-            rewrite.reset_parameters()
-            tick("└─ reset parameters")
-            start_pos = idx
-    if not found:
+        matched = a.match(pattern, browse=True, pos=start_pos)
+        if matched is None:
+            break
+        found_match = True
+        idx = a.isolate(list(matched.pos_map.keys()), color=color)
+        for k, v in matched.v_map.items():
+            pattern[k].set_value(v)
+        v = pattern.compute_unitary(False)
+        res = optimize(rewrite, v, frobenius, sign=-1)
+        subc = rewrite.copy()
+        found = True
+        a.replace(idx, subc, merge=False)
+        a.replace(idx, subc, merge=True)
+        pattern.reset_parameters()
+        rewrite.reset_parameters()
+        start_pos = idx
+        print(pattern._name, res.fun, fidelity(u, a.compute_unitary(False)))
+    if not found_match:
         break
