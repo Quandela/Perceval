f3dc900b0ddf8445932d105c99f8d35f0dc9c132
Jean Senellart <jean.senellart@quandela.com>
2022-09-10 23:11:25 +0200

    fix additional tests

--- tests/test_permutation.py
+++ tests/test_permutation.py
@@ -28,16 +28,13 @@ def test_permutation_3():
     circuit = comp.PERM([2, 0, 1])
     simulator_backend = pcvl.BackendFactory().get_backend("SLOS")
     s_circuit = simulator_backend(circuit)
-    ca = pcvl.CircuitAnalyser(s_circuit, input_states=[pcvl.AnnotatedBasicState("|1,0,0>")],
-                              output_states="*")
+    ca = pcvl.CircuitAnalyser(s_circuit, input_states=[pcvl.BasicState("|1,0,0>")], output_states="*")
     assert ca.output_states_list[2] == pcvl.BasicState("|0, 0, 1>")
     assert not((ca.distribution[0]-[0, 0, 1]).any())
-    ca = pcvl.CircuitAnalyser(s_circuit, input_states=[pcvl.AnnotatedBasicState("|0,1,0>")],
-                              output_states="*")
+    ca = pcvl.CircuitAnalyser(s_circuit, input_states=[pcvl.BasicState("|0,1,0>")], output_states="*")
     assert ca.output_states_list[0] == pcvl.BasicState("|1, 0, 0>")
     assert not((ca.distribution[0]-[1, 0, 0]).any())
-    ca = pcvl.CircuitAnalyser(s_circuit, input_states=[pcvl.AnnotatedBasicState("|0,0,1>")],
-                              output_states="*")
+    ca = pcvl.CircuitAnalyser(s_circuit, input_states=[pcvl.BasicState("|0,0,1>")], output_states="*")
     assert ca.output_states_list[1] == pcvl.BasicState("|0, 1, 0>")
     assert not((ca.distribution[0]-[0, 1, 0]).any())
 
