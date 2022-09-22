c333ca954b63f26c313cd7977fb6e3c38dc3f6cb
Aurélien Baert <aurelien.baert@quandela.com>
2022-09-22 16:03:09 +0200

    Re exposed Annotation as it is needed for autotests

--- perceval/utils/__init__.py
+++ perceval/utils/__init__.py
@@ -24,7 +24,7 @@ from .matrix import Matrix, MatrixN, MatrixS
 from .format import simple_float, simple_complex, format_parameters
 from .parameter import Parameter, P, Expression, E
 from .mlstr import mlstr
-from .statevector import BasicState, StateVector, SVDistribution, tensorproduct, AnnotatedBasicState
+from .statevector import BasicState, StateVector, SVDistribution, tensorproduct, AnnotatedBasicState, Annotation
 from .polarization import Polarization
 from .random import random_seed
 from .globals import global_params
