68f381b3f852df717c88958ae6aca02592648447
Eric Bertasi <eric.bertasi@quandela.com>
2022-11-17 14:24:22 +0100

    Fix serialize_to_file and deserialize_file

--- perceval/serialization/__init__.py
+++ perceval/serialization/__init__.py
@@ -23,5 +23,4 @@
 from .serialize import serialize, serialize_to_file
 from ._state_serialization import deserialize_state, deserialize_state_list
 from .deserialize import deserialize, deserialize_circuit, circuit_from_file, deserialize_matrix, matrix_from_file, \
-      deserialize_float
-      # , deserialize_sample_count, sample_count_from_file
+      deserialize_float, deserialize_file
