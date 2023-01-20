997fa1c0117dd9573d20c49b05f7b48e179899b2
Eric Bertasi <49883965+ericbrts@users.noreply.github.com>
2023-01-20 17:57:53 +0100

    PCVL-344 binary serialization (#167)

--- perceval/serialization/deserialize.py
+++ perceval/serialization/deserialize.py
@@ -33,6 +33,10 @@ from perceval.serialization import _schema_circuit_pb2 as pb
 from base64 import b64decode
 
 
+_MATRIX_PREFIX = ":PCVL:Matrix:"
+_CIRCUIT_PREFIX = ":PCVL:ACircuit:"
+
+
 def deserialize_float(floatstring):
     return float(floatstring)
 
@@ -41,8 +45,11 @@ def deserialize_matrix(pb_mat: Union[str, pb.Matrix]) -> Matrix:
     if not isinstance(pb_mat, pb.Matrix):
         pb_binary_repr = pb_mat
         pb_mat = pb.Matrix()
-        assert pb_binary_repr.startswith(":PCVL:Matrix:")
-        pb_mat.ParseFromString(b64decode(pb_binary_repr[13:]))
+        if isinstance(pb_binary_repr, bytes):
+            pb_mat.ParseFromString(pb_binary_repr)
+        else:
+            assert pb_binary_repr.startswith(_MATRIX_PREFIX)
+            pb_mat.ParseFromString(b64decode(pb_binary_repr[len(_MATRIX_PREFIX):]))
     return _matrix_serialization.deserialize_pb_matrix(pb_mat)
 
 
@@ -56,12 +63,15 @@ def matrix_from_file(filepath: str) -> Matrix:
         return deserialize_matrix(f.read())
 
 
-def deserialize_circuit(pb_circ: Union[str, pb.Circuit]) -> Circuit:
+def deserialize_circuit(pb_circ: Union[str, bytes, pb.Circuit]) -> Circuit:
     if not isinstance(pb_circ, pb.Circuit):
         pb_binary_repr = pb_circ
         pb_circ = pb.Circuit()
-        assert pb_binary_repr.startswith(":PCVL:ACircuit:")
-        pb_circ.ParseFromString(b64decode(pb_binary_repr[15:]))
+        if isinstance(pb_binary_repr, bytes):
+            pb_circ.ParseFromString(pb_binary_repr)
+        else:
+            assert pb_binary_repr.startswith(_CIRCUIT_PREFIX)
+            pb_circ.ParseFromString(b64decode(pb_binary_repr[len(_CIRCUIT_PREFIX):]))
     builder = CircuitBuilder(pb_circ.n_mode, pb_circ.name)
     for pb_c in pb_circ.components:
         builder.add(pb_c)
@@ -106,6 +116,9 @@ def deserialize_bscount(serial_bsc):
 
 
 def deserialize(obj):
+    if isinstance(obj, bytes):
+        raise TypeError("Generic deserialize function does not handle binary representation. "
+                        "Use specialized functions (e.g. deserialize_circuit) instead.")
     if isinstance(obj, dict):
         r = {}
         for k, v in obj.items():
