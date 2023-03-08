bda5e783b54dc6c2aa8affcab45e2e4e4a345108
Eric Bertasi <eric.bertasi@quandela.com>
2023-03-08 15:57:37 +0100

    perceval.utils exports Annotation class

--- perceval/utils/__init__.py
+++ perceval/utils/__init__.py
@@ -25,9 +25,10 @@ from .format import simple_float, simple_complex, format_parameters
 from .parameter import Parameter, P, Expression, E
 from .mlstr import mlstr
 from .statevector import BasicState, StateVector, SVDistribution, BSDistribution, BSCount, BSSamples, \
-    tensorproduct, AnnotatedBasicState, Annotation, allstate_iterator
+    tensorproduct, AnnotatedBasicState, allstate_iterator
 from .polarization import Polarization
 from .random import random_seed
 from .globals import global_params
 from .conversion import samples_to_sample_count, samples_to_probs, sample_count_to_samples, sample_count_to_probs,\
     probs_to_samples, probs_to_sample_count
+from exqalibur import Annotation  # Used to provide the Annotation class to the perceval root namespace
