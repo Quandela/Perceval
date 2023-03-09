6e2bc469d8e005ac1ec1f93436f8b656dfccd73a
Eric Bertasi <eric.bertasi@quandela.com>
2023-03-09 16:51:49 +0100

    Update Perceval license

--- perceval/algorithm/qrng.py
+++ perceval/algorithm/qrng.py
@@ -12,6 +12,13 @@
 # The above copyright notice and this permission notice shall be included in all
 # copies or substantial portions of the Software.
 #
+# As a special exception, the copyright holders of exqalibur library give you
+# permission to combine exqalibur with code included in the standard release of
+# Perceval under the MIT license (or modified versions of such code). You may
+# copy and distribute such a combined system following the terms of the MIT
+# license for both exqalibur and Perceval. This exception for the usage of
+# exqalibur is limited to the python bindings used by Perceval.
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -43,4 +50,3 @@ class QRNG(AAlgorithm):
             "Qrng algorithm is not implemented"
         return RemoteJob(lambda **kwargs: self._processor.async_execute("qrng", **kwargs),
                          self._processor.get_rpc_handler())
-
