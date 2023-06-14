f000e2abbc083cee8df47f1f9d491605a369ee46
lukamusic <luka.music@quandela.com>
2023-06-14 18:38:48 +0200

    corrected indentation

--- perceval/components/core_catalog/heralded_cz.py
+++ perceval/components/core_catalog/heralded_cz.py
@@ -36,7 +36,7 @@ ctrl (dual rail) ─────┤     ├───── ctrl (dual rail)
 data (dual rail) ─────┤     ├───── data (dual rail)
                  ─────┤     ├─────
                       ╰─────╯"""
-
+	
     theta_1 = 2*np.pi*54.74/180
     theta_2 = 2*np.pi*17.63/180
 
@@ -46,26 +46,26 @@ data (dual rail) ─────┤     ├───── data (dual rail)
 
     def build(self):
 	#the matrix of this first circuit is the same as the one presented in the reference paper, the difference in the second phase shift - placed on mode 3 instead of mode 1 - is due to a different convention for the beam-splitters (signs inverted in second column).
-	last_modes_cz = (Circuit(4)
-		.add(0, PS(np.pi))
-		.add(3, PS(np.pi))
-		.add((1, 2), PERM([1, 0]))
-		.add((0, 1), BS.H(theta=self.theta1))
-		.add((2, 3), BS.H(theta=self.theta1))
-		.add((1, 2), PERM([1, 0]))
-		.add((0, 1), BS.H(theta=-self.theta1))
-		.add((2, 3), BS.H(theta=self.theta2)))
+    last_modes_cz = (Circuit(4)
+        .add(0, PS(np.pi))
+        .add(3, PS(np.pi))
+        .add((1, 2), PERM([1, 0]))
+        .add((0, 1), BS.H(theta=self.theta1))
+        .add((2, 3), BS.H(theta=self.theta1))
+        .add((1, 2), PERM([1, 0]))
+        .add((0, 1), BS.H(theta=-self.theta1))
+        .add((2, 3), BS.H(theta=self.theta2)))
 
-	c_hcz=(Circuit(6, name="Heralded CZ")
-		.add((1, 2), PERM([1, 0]))
-		.add((2, 3, 4, 5), last_modes_cz)
-		.add((1, 2), PERM([1, 0])))
+    c_hcz=(Circuit(6, name="Heralded CZ")
+        .add((1, 2), PERM([1, 0]))
+        .add((2, 3, 4, 5), last_modes_cz)
+        .add((1, 2), PERM([1, 0])))
 
-        if self._opt('type') == AsType.CIRCUIT:
-            return c_hcz
-        elif self._opt('type') == AsType.PROCESSOR:
-            p = Processor(self._opt('backend'), c_hcz)
-            return p.add_port(0, Port(Encoding.DUAL_RAIL, 'ctrl')) \
-                    .add_port(2, Port(Encoding.DUAL_RAIL, 'data')) \
-                    .add_herald(4, 1) \
-                    .add_herald(5, 1)
+    if self._opt('type') == AsType.CIRCUIT:
+        return c_hcz
+    elif self._opt('type') == AsType.PROCESSOR:
+        p = Processor(self._opt('backend'), c_hcz)
+        return p.add_port(0, Port(Encoding.DUAL_RAIL, 'ctrl')) \
+                .add_port(2, Port(Encoding.DUAL_RAIL, 'data')) \
+                .add_herald(4, 1) \
+                .add_herald(5, 1)
