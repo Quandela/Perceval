6f47a6d8f72ca80cde571ea24e5c13831b7511ff
Eric Bertasi <eric.bertasi@quandela.com>
2023-07-03 11:25:07 +0200

    In Dictionary and List serializers:
    * compression now transfer "compress" parameter
    * behavior may differ given the serialized object type

--- perceval/serialization/serialize.py
+++ perceval/serialization/serialize.py
@@ -29,7 +29,7 @@
 
 from multipledispatch import dispatch
 from functools import wraps
-from zlib import compress
+from zlib import compress as zlib_compress
 
 from ._matrix_serialization import serialize_matrix
 from ._circuit_serialization import serialize_circuit
@@ -41,98 +41,111 @@ from base64 import b64encode
 import json
 
 
-def to_compress(func):
-    @wraps(func)
-    def compressor(*args, **kwargs):
-        if kwargs.get('compress', False):
-            serialized_string = func(*args, **kwargs)  # serialized obj: string format
-            serialized_string_compressed = compress(serialized_string.encode('utf-8'))
-            # serialized and compressed : byte format
-            serialized_string_compressed_byt2str = b64encode(serialized_string_compressed).decode('utf-8')
-            # serialized and compressed : string format
-            serialized_compressed_byt2str_zip_prefix = ":PCVL:zip:" + serialized_string_compressed_byt2str
-            # adding zip prefix
-            return serialized_compressed_byt2str_zip_prefix
-        else:
-            return func(*args, **kwargs)
-    return compressor
+@dispatch(bool, str)
+def _handle_compress_parameter(compress, type_str) -> bool:
+    return compress
 
 
-@to_compress
-@dispatch(ACircuit)
+@dispatch(list, str)
+def _handle_compress_parameter(compress, type_str) -> bool:
+    return type_str in compress
+
+
+def _handle_compression(serialized_obj: str, do_compress: bool) -> str:
+    if not do_compress:
+        return serialized_obj
+    serialized_string_compressed = zlib_compress(serialized_obj.encode('utf-8'))  # Compress byte to byte
+    serialized_string_compressed_byt2str = b64encode(serialized_string_compressed).decode('utf-8')  # base64 to string
+    return ":PCVL:zip:" + serialized_string_compressed_byt2str
+
+
+@dispatch(ACircuit, compress=(list, bool))
 def serialize(circuit: ACircuit, compress=True) -> str:
-    return ":PCVL:ACircuit:" + b64encode(serialize_circuit(circuit).SerializeToString()).decode('utf-8')
+    tag = 'ACircuit'
+    compress = _handle_compress_parameter(compress, tag)
+    return _handle_compression(
+        f":PCVL:{tag}:" + b64encode(serialize_circuit(circuit).SerializeToString()).decode('utf-8'),
+        do_compress=compress)
 
 
-@to_compress
-@dispatch(Matrix)
+@dispatch(Matrix, compress=(list, bool))
 def serialize(m: Matrix, compress=False) -> str:
-    return ":PCVL:Matrix:" + b64encode(serialize_matrix(m).SerializeToString()).decode('utf-8')
+    tag = "Matrix"
+    compress = _handle_compress_parameter(compress, tag)
+    return _handle_compression(f":PCVL:{tag}:" + b64encode(serialize_matrix(m).SerializeToString()).decode('utf-8'),
+                               do_compress=compress)
 
 
-@to_compress
-@dispatch(BasicState)
+@dispatch(BasicState, compress=(list, bool))
 def serialize(obj, compress=False) -> str:
-    return ":PCVL:BasicState:" + serialize_state(obj)
+    tag = "BasicState"
+    compress = _handle_compress_parameter(compress, tag)
+    return _handle_compression(f":PCVL:{tag}:" + serialize_state(obj), do_compress=compress)
 
 
-@to_compress
-@dispatch(StateVector)
+
+@dispatch(StateVector, compress=(list, bool))
 def serialize(sv, compress=False) -> str:
-    return ":PCVL:StateVector:" + serialize_statevector(sv)
+    tag = "StateVector"
+    compress = _handle_compress_parameter(compress, tag)
+    return _handle_compression(f":PCVL:{tag}:" + serialize_statevector(sv), do_compress=compress)
 
 
-@to_compress
-@dispatch(SVDistribution)
-def serialize(dist, compress=False) -> str:
-    return ":PCVL:SVDistribution:{" \
+@dispatch(SVDistribution, compress=(list, bool))
+def serialize(dist: SVDistribution, compress=False) -> str:
+    tag = "SVDistribution"
+    compress = _handle_compress_parameter(compress, tag)
+    serial_svd = f":PCVL:{tag}:{{" \
            + ";".join(["%s=%s" % (serialize_statevector(k), simple_float(v, nsimplify=False)[1])
                        for k, v in dist.items()]) \
            + "}"
+    return _handle_compression(serial_svd, do_compress=compress)
 
 
-@to_compress
-@dispatch(BSDistribution)
-def serialize(dist, compress=False) -> str:
-    return ":PCVL:BSDistribution:{" \
+@dispatch(BSDistribution, compress=(list, bool))
+def serialize(dist: BSDistribution, compress=False) -> str:
+    tag = "BSDistribution"
+    compress = _handle_compress_parameter(compress, tag)
+    serial_bsd = f":PCVL:{tag}:{{" \
            + ";".join(["%s=%s" % (serialize_state(k), simple_float(v, nsimplify=False)[1]) for k, v in dist.items()]) \
            + "}"
+    return _handle_compression(serial_bsd, do_compress=compress)
 
 
-@to_compress
-@dispatch(BSCount)
+@dispatch(BSCount, compress=(list, bool))
 def serialize(obj, compress=False) -> str:
-    return ":PCVL:BSCount:{" \
+    tag = "BSCount"
+    compress = _handle_compress_parameter(compress, tag)
+    serial_bsc = f":PCVL:{tag}:{{" \
            + ";".join(["%s=%s" % (serialize_state(k), str(v)) for k, v in obj.items()]) \
            + "}"
+    return _handle_compression(serial_bsc, do_compress=compress)
 
 
-@to_compress
-@dispatch(BSSamples)
-def serialize(obj, compress=False) -> str:
-    return ":PCVL:BSSamples:" + serialize_bssamples(obj)
+@dispatch(BSSamples, compress=(list, bool))
+def serialize(obj, compress=True) -> str:
+    tag = "BSSamples"
+    compress = _handle_compress_parameter(compress, tag)
+    return _handle_compression(f":PCVL:{tag}:" + serialize_bssamples(obj), do_compress=compress)
 
 
-@to_compress
-@dispatch(dict)
+@dispatch(dict, compress=(list, bool))
 def serialize(obj, compress=False) -> dict:
     r = {}
     for k, v in obj.items():
-        r[serialize(k)] = serialize(v)
+        r[serialize(k, compress=compress)] = serialize(v, compress=compress)
     return r
 
 
-@to_compress
-@dispatch(list)
+@dispatch(list, compress=(list, bool))
 def serialize(obj, compress=False) -> list:
     r = []
     for k in obj:
-        r.append(serialize(k))
+        r.append(serialize(k, compress=compress))
     return r
 
 
-@to_compress
-@dispatch(object)
+@dispatch(object, compress=(list, bool))
 def serialize(obj, compress=False) -> object:
     return obj
 
