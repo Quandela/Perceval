c29a9d8b2401479f92d637923f2508fb8ca6d7d4
Marion Fabre <marion.fabre@quandela.com>
2023-07-07 16:54:38 +0200

    change ci

--- .github/workflows/rerun_notebooks.sh
+++ .github/workflows/rerun_notebooks.sh
@@ -1,35 +1,36 @@
 
 my_function () {
-    echo "$notebook"
-    jupyter nbconvert --clear-output --inplace "$notebook"
-    jupyter nbconvert --execute --to notebook "$notebook"
+    jupyter nbconvert --clear-output --inplace "$notebook.ipynb"
+    jupyter nbconvert --execute --to notebook "$notebook.ipynb"
+    rm "$notebook.ipynb"
+    mv "$notebook.nbconvert.ipynb" "$notebook.ipynb"
 }
 
 cd docs/source/notebooks/
 
-notebook='2-mode Grover algorithm.ipynb'
+notebook='2-mode Grover algorithm'
 my_function
-notebook='Non-unitary components.ipynb' 
+notebook='Non-unitary components' 
 my_function
-notebook='Shor Implementation.ipynb' 
+notebook='Shor Implementation' 
 my_function
-notebook='QUBO.ipynb' 
+notebook='QUBO' 
 my_function
-notebook='Tutorial.ipynb' 
+notebook='Tutorial' 
 my_function
-notebook='Boson Sampling with MPS.ipynb' 
+notebook='Boson Sampling with MPS' 
 my_function
-notebook='Qiskit conversion.ipynb' 
+notebook='Qiskit conversion' 
 my_function
-notebook='Variational Quantum Eigensolver.ipynb' 
+notebook='Variational Quantum Eigensolver' 
 my_function
-notebook='Reinforcement learning.ipynb' 
+notebook='Reinforcement learning' 
 my_function
-notebook='Differential equation solving.ipynb'
+notebook='Differential equation solving'
 my_function
-notebook='walkthrough-cnot.ipynb' 
+notebook='walkthrough-cnot' 
 my_function
-notebook='Graph States and representation.ipynb' 
+notebook='Graph States and representation' 
 my_function
-notebook='Rewriting rules in Perceval.ipynb'
+notebook='Rewriting rules in Perceval'
 my_function
