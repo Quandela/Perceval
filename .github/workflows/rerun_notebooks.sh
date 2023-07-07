6225aa065033684a545951828ec06127791cc7f3
Marion Fabre <marion.fabre@quandela.com>
2023-07-07 14:33:08 +0200

    Add notebooks ci

---
+++ .github/workflows/rerun_notebooks.sh
@@ -0,0 +1,37 @@
+
+my_function () {
+    echo "$notebook"
+    jupyter nbconvert --clear-output --inplace "$notebook"
+    jupyter nbconvert --execute --to notebook "$notebook"
+}
+
+cd docs/source/notebooks/
+
+notebook='2-mode Grover algorithm.ipynb'
+my_function
+notebook='Non-unitary components.ipynb' 
+my_function
+notebook='Shor Implementation.ipynb' 
+my_function
+notebook='QUBO.ipynb' 
+my_function
+notebook='Tutorial.ipynb' 
+my_function
+notebook='Boson Sampling with MPS.ipynb' 
+my_function
+notebook='Qiskit conversion.ipynb' 
+my_function
+notebook='Variational Quantum Eigensolver.ipynb' 
+my_function
+notebook='Reinforcement learning.ipynb' 
+my_function
+notebook='Differential equation solving.ipynb'
+my_function 
+notebook='Remote computing.ipynb' 
+my_function
+notebook='walkthrough-cnot.ipynb' 
+my_function
+notebook='Graph States and representation.ipynb' 
+my_function
+notebook='Rewriting rules in Perceval.ipynb'
+my_function
--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     packages=package_list,
-    install_requires=['sympy', 'numpy', 'scipy', 'tabulate', 'matplotlib', 'exqalibur~=0.2.0', 'multipledispatch',
+    install_requires=['sympy', 'numpy', 'scipy', 'tabulate~=0.8.9', 'matplotlib', 'exqalibur~=0.2.0', 'multipledispatch',
                       'protobuf>=3.20.3', 'drawsvg>=2.0', 'Deprecated', 'requests', 'networkx~=3.1', 'latexcodec'],
     setup_requires=["scmver"],
     python_requires=">=3.7",
