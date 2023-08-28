e3258db0e5ed0cd2370a9bdc36be9150b6e7dbfb
Marion Fabre <marion.fabre@quandela.com>
2023-08-28 16:52:53 +0200

    implement system features

--- perceval/utils/persistent_data.py
+++ perceval/utils/persistent_data.py
@@ -27,14 +27,124 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+import os
+import warnings
 from platformdirs import PlatformDirs
+
 from .metadata import PMetadata
 
 
-class PersistentData():
+class PersistentData:
+    """PersistentData handle perceval persistent data
+    On init, it creates a directory (if it doesn't exist) for storing perceval persistent data
+    Directory depends of the os:
+    * Linux: '/home/my_user/.local/share/perceval-quandela'
+    * Windows: 'C:\\Users\\my_user\\AppData\\Local\\quandela\\perceval-quandela'
+    * Darwin: '/Users/my_user/Library/Application Support/perceval-quandela'
+
+    If the directory cannot be created or read/write in, a warning will inform the user
+    """
     def __init__(self) -> None:
         self._directory = PlatformDirs(PMetadata.package_name(), PMetadata.author()).user_data_dir
+        try:
+            self._create_directory()
+        except OSError as exc:
+            warnings.warn(exc)
+            return
+        if not self.is_writable() or not self.is_readable():
+            warnings.warn(UserWarning(f"Cannot read or write in {self._directory}"))
+
+    def is_writable(self) -> bool:
+        """Return if the directory is writable
+
+        :return: True if the directory is writable, False else
+        """
+        return os.access(self._directory, os.W_OK)
+
+    def is_readable(self) -> bool:
+        """Return if the directory is readable
+
+        :return: True if the directory is readable, False else
+        """
+        return os.access(self._directory, os.R_OK)
+
+    def _create_directory(self) -> None:
+        """Create the directory if if doesn't exists
+        """
+        if not os.path.exists(self._directory):
+            os.makedirs(self._directory)
+
+    def get_data_size(self):
+        """Get the directory data size
+
+        :return: directory data size
+        """
+        return sum(os.path.getsize(os.path.join(dirpath, filename))
+                   for dirpath, dirnames, filenames in os.walk(self._directory) for filename in filenames)
+
+    def _get_full_path(self, element_name: str) -> str:
+        """Get the full path of an element supposedly in persistent data directory
+
+        :param element_name: name of the element (with extension)
+        :return: full path of the file
+        """
+        return os.path.join(self._directory, element_name)
+
+    def create_file(self, filename: str):
+        """Create a file in persistent data directory
+        if file already exist, raise a user warning
+
+        :param filename: name of the file to create (with extension)
+        """
+        file_path = self._get_full_path(filename)
+        if os.path.exists(file_path):
+            warnings.warn(UserWarning(f"Cannot create {file_path}, file already exist"))
+            return
+        with open(file_path, "bx") as file:
+            file.write(b'')
+
+    def delete_file(self, filename: str):
+        """Delete a file in persistent data directory
+        if file doesn't exist, raise a user warning
+
+        :param filename: name of the file to delete (with extension)
+        """
+        file_path = self._get_full_path(filename)
+        if not os.path.exists(file_path):
+            warnings.warn(UserWarning(f"Cannot delete {file_path}, file doesn't exist"))
+            return
+        os.remove(file_path)
+
+    def write_binary_file(self, filename: str, data: bytes):
+        """Write binary data into a file in persistent data directory
+
+        :param filename: name of the file to write in (with extension)
+        :param data: data to write
+        """
+        file_path = self._get_full_path(filename)
+        with open(file_path, "wb") as file:
+            file.write(data)
+
+    def read_binary_file(self, filename: str) -> bytes:
+        """Read binary data from a file in persistent data directory
+
+        :param filename: name of the file to read (with extension)
+        :raises FileNotFoundError: Raise an exception if file is not found
+        :return: data
+        """
+        file_path = self._get_full_path(filename)
+        if not os.path.exists(file_path):
+            raise FileNotFoundError(file_path)
+        data = None
+        with open(file_path, "r+b") as file:
+            data = file.read()
+        data = data.removesuffix(b'\n').removesuffix(b' ')
+        return data
 
     @property
     def directory(self) -> str:
+        """return persistent data directory
+
+        :return: persistent data directory
+        """
         return self._directory
--- tests/test_persistent_data.py
+++ tests/test_persistent_data.py
@@ -27,13 +27,15 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-import platform
+import os
 import re
+import pytest
+import platform
 
 from perceval.utils import PersistentData
 
 
-def test_persistent_data():
+def test_directory():
     pd = PersistentData()
     if platform.system() == "Linux":
         # '/home/my_user/.local/share/perceval-quandela'
@@ -46,3 +48,55 @@ def test_persistent_data():
         assert re.match(r"^\/Users\/.+\/Library\/Application Support\/perceval-quandela$", pd.directory) is not None
     else:
         raise OSError("My god where are you ?")
+
+
+def test_basic_methods():
+    persistent_data = PersistentData()
+    assert os.path.exists(persistent_data.directory)
+    assert persistent_data.is_writable()
+    assert persistent_data.is_readable()
+
+    persistent_data.create_file("toto")
+    assert os.path.exists(os.path.join(persistent_data.directory, "toto"))
+    with pytest.warns(UserWarning):
+        persistent_data.create_file("toto")
+
+    persistent_data.delete_file("toto")
+    assert not os.path.exists(os.path.join(persistent_data.directory, "toto"))
+    with pytest.warns(UserWarning):
+        persistent_data.delete_file("toto")
+    with pytest.raises(FileNotFoundError):
+        persistent_data.read_binary_file("toto")
+
+    persistent_data.write_binary_file("toto", b"DEADBEEFDEADBEEF")
+    assert persistent_data.read_binary_file("toto") == b"DEADBEEFDEADBEEF"
+    persistent_data.delete_file("toto")
+
+
+def test_access():
+    persistent_data = PersistentData()
+    directory = persistent_data.directory
+
+    os.chmod(directory, 0o000)
+    assert not persistent_data.is_writable()
+    assert not persistent_data.is_readable()
+
+    os.chmod(directory, 0o444)
+    assert not persistent_data.is_writable()
+    assert persistent_data.is_readable()
+
+    os.chmod(directory, 0o777)
+    assert persistent_data.is_writable()
+    assert persistent_data.is_readable()
+
+    parent_directory = os.path.dirname(directory)
+
+    os.chmod(parent_directory, 0o000)
+    with pytest.warns(UserWarning):
+        PersistentData()
+
+    os.chmod(parent_directory, 0o444)
+    with pytest.warns(UserWarning):
+        PersistentData()
+
+    os.chmod(parent_directory, 0o777)
