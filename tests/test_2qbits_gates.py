fef3eb447728094b23f1afcfe8329fe20ca349c2
Marion Fabre <marion.fabre@quandela.com>
2023-09-04 16:51:22 +0200

    Split gate tests into several files, remove useless tests

--- tests/test_2qbits_gates.py
+++ tests/test_2qbits_gates.py
@@ -30,37 +30,60 @@
 import pytest
 
 import perceval as pcvl
-from perceval.components import catalog
-from perceval.algorithm import Analyzer
+from perceval import BasicState
+from perceval.components import catalog, BS, PERM
+from perceval.algorithm import Analyzer, Sampler
 
 
-def test_performance_compare_cnot():
-    # Tests the performance of different CNOT in perceval
+def test_fidelity_and_performance_compare_cnot():
+    # Tests the performance and the fidelity of different CNOT in perceval
     # KLM CNOT
     klm_cnot = catalog["klm cnot"].build_processor()
     state_dict = {pcvl.components.get_basic_state_from_ports(klm_cnot._out_ports, state): str(
         state) for state in pcvl.utils.generate_all_logical_states(2)}
     analyzer_klm_cnot = Analyzer(klm_cnot, state_dict)
     analyzer_klm_cnot.compute(expected={"00": "00", "01": "01", "10": "11", "11": "10"})
-    assert pytest.approx(analyzer_klm_cnot.fidelity, 10E-5) == 1
     analyzer_klm_cnot_perf = pcvl.simple_float(analyzer_klm_cnot.performance)[1]
+
+    assert pytest.approx(analyzer_klm_cnot.fidelity, 10E-5) == 1
 
     # Postprocessed CNOT
     postprocessed_cnot = catalog["postprocessed cnot"].build_processor()
-    state_dict = {pcvl.components.get_basic_state_from_ports(postprocessed_cnot._out_ports, state): str(
-        state) for state in pcvl.utils.generate_all_logical_states(2)}
     analyzer_postprocessed_cnot = Analyzer(postprocessed_cnot, state_dict)
     analyzer_postprocessed_cnot.compute(expected={"00": "00", "01": "01", "10": "11", "11": "10"})
-    assert analyzer_postprocessed_cnot.fidelity == 1
     analyzer_postprocessed_cnot_perf = pcvl.simple_float(analyzer_postprocessed_cnot.performance)[1]
+
+    assert analyzer_postprocessed_cnot.fidelity == 1
 
     # CNOT using CZ : called - Heralded CNOT
     heralded_cnot = catalog["heralded cnot"].build_processor()
-    state_dict = {pcvl.components.get_basic_state_from_ports(heralded_cnot._out_ports, state): str(
-        state) for state in pcvl.utils.generate_all_logical_states(2)}
     analyzer_heralded_cnot = Analyzer(heralded_cnot, state_dict)
     analyzer_heralded_cnot.compute(expected={"00": "00", "01": "01", "10": "11", "11": "10"})
-    assert pytest.approx(analyzer_heralded_cnot.fidelity) == 1
     analyzer_heralded_cnot_perf = pcvl.simple_float(analyzer_heralded_cnot.performance)[1]
 
+    assert pytest.approx(analyzer_heralded_cnot.fidelity) == 1
+
     assert analyzer_postprocessed_cnot_perf > analyzer_heralded_cnot_perf > analyzer_klm_cnot_perf
+
+
+# We could use H gates on both size of a CZ gate to check CZ phase
+# but since our heralded CNOT is built this way, we don't need to
+@pytest.mark.parametrize("cnot_gate", ["klm cnot", "postprocessed cnot", "heralded cnot"])
+def test_cnot_phase(cnot_gate):
+    processor = pcvl.Processor("SLOS", 4)
+    processor.add([2, 3], PERM([1, 0]))
+    processor.add([0, 1], BS.H())
+    processor.add([2, 3], BS.H())
+    # Commented lines are use to compare with a26b0bd (0.8.1 before cnot fix)
+    # processor.add([0, 1, 2, 3], catalog["postprocessed cnot"].as_processor().build()) # < 0.9.0
+    # processor.clear_postprocess() # < 0.9.0
+    processor.add([0, 1, 2, 3], catalog[cnot_gate].build_processor())  # >= 0.9.0
+    processor.add([0, 1], BS.H())
+    processor.add([2, 3], BS.H())
+    # processor.set_postprocess(lambda o: (o[0] + o[1] == 1) and (o[2] + o[3] == 1)) # < 0.9.0
+
+    processor.with_input(BasicState([1, 0, 1, 0]))
+    sampler = Sampler(processor)
+    nb_sample = 10
+    samples = sampler.samples(nb_sample)
+    assert samples['results'].count(BasicState([0, 1, 0, 1])) == nb_sample
---
+++ tests/test_3qbits_gates.py
@@ -0,0 +1,136 @@
+# MIT License
+#
+# Copyright (c) 2022 Quandela
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# As a special exception, the copyright holders of exqalibur library give you
+# permission to combine exqalibur with code included in the standard release of
+# Perceval under the MIT license (or modified versions of such code). You may
+# copy and distribute such a combined system following the terms of the MIT
+# license for both exqalibur and Perceval. This exception for the usage of
+# exqalibur is limited to the python bindings used by Perceval.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+import pytest
+from math import pi, sqrt
+
+import perceval as pcvl
+from perceval import PostSelect, Matrix, BasicState
+from perceval.algorithm import Analyzer, Sampler
+from perceval.utils import Encoding, generate_all_logical_states
+from perceval.components import catalog, BS, PERM, Unitary, PS, Port, get_basic_state_from_ports
+
+
+def test_toffoli_gate_fidelity():
+    t = catalog['toffoli'].build_processor()
+    state_dict = {get_basic_state_from_ports(t._out_ports, state): str(
+        state) for state in generate_all_logical_states(3)}
+    ca = Analyzer(t, input_states=state_dict)
+    ca.compute(expected={"000": "000", "001": "001", "010": "010", "011": "011",
+               "100": "100", "101": "101", "110": "111", "111": "110"})
+    assert ca.fidelity == 1
+
+
+@pytest.mark.skip(reason="Work in progress")
+def test_toffoli_phase():
+    processor = pcvl.Processor("SLOS", 6)
+    processor.add([2, 3], PERM([1, 0]))
+    processor.add([4, 5], PERM([1, 0]))
+    processor.add([0, 1], BS.H())
+    processor.add([2, 3], BS.H())
+    processor.add([4, 5], BS.H())
+    processor.add([0, 1, 2, 3, 4, 5], catalog["toffoli"].build_processor())
+    processor.add([0, 1], BS.H())
+    processor.add([2, 3], BS.H())
+    processor.add([4, 5], BS.H())
+
+    processor.with_input(BasicState([1, 0, 1, 0, 1, 0]))
+    sampler = Sampler(processor)
+    nb_sample = 10
+    samples = sampler.samples(nb_sample)
+    assert samples['results'].count(BasicState([0, 1, 0, 1, 0, 1])) == nb_sample
+
+
+def test_full_toffoli():
+    a = 1 / sqrt(2)
+    H3 = Matrix([[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, a, 0, 0, 0, 0, 0, 0, 0, a, 0],
+                 [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+                 [0, 0, a, 0, 0, 0, 0, 0, 0, 0, -a, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]])
+    Z3 = Matrix([[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]])
+    # U is obtained by a method not communicated
+    U = Matrix([[0.509824528533959, 0.321169327626332 + 0.556281593281541j, 0, 0.330393705586394, -0.165196852793197 - 0.286129342288294j, -0.165196852793197 + 0.286129342288294j, 0, 0, 0, 0, 0, 0],
+                [0, 0.509824528533959, 0.321169327626332 + 0.556281593281541j, -0.165196852793197
+                 + 0.286129342288294j, 0.330393705586394, -0.165196852793197 - 0.286129342288294j, 0, 0, 0, 0, 0, 0],
+                [0.321169327626332 + 0.556281593281541j, 0, 0.509824528533959, -0.165196852793197
+                 - 0.286129342288294j, -0.165196852793197 + 0.286129342288294j, 0.330393705586394, 0, 0, 0, 0, 0, 0],
+                [0.330393705586394, -0.165196852793197 - 0.286129342288294j, -0.165196852793197
+                 + 0.286129342288294j, -0.509824528533959, 0, -0.321169327626332 + 0.556281593281541j, 0, 0, 0, 0, 0, 0],
+                [-0.165196852793197 + 0.286129342288294j, 0.330393705586394, -0.165196852793197
+                 - 0.286129342288294j, -0.321169327626332 + 0.556281593281541j, -0.509824528533959, 0, 0, 0, 0, 0, 0, 0],
+                [-0.165196852793197 - 0.286129342288294j, -0.165196852793197 + 0.286129342288294j, 0.330393705586394,
+                 0, -0.321169327626332 + 0.556281593281541j, -0.509824528533959, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0.509824528533959, 0.860278414296864, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0.860278414296864, -0.509824528533959, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0.509824528533959, 0.860278414296864, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0.860278414296864, -0.509824528533959, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.509824528533959, 0.860278414296864],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.860278414296864, -0.509824528533959]])
+    Pin = PERM([6, 0, 8, 1, 10, 2, 3, 4, 5, 7, 9, 11])
+    Pout = PERM([1, 3, 5, 6, 7, 8, 0, 9, 2, 10, 4, 11])
+    toffoli = Pin // Unitary(Z3 @ H3 @ U @ H3 @ Z3) // Pout
+    toffoli.add(2, PS(pi))
+    toffoli.add(0, PS(pi / 2))
+
+    processor = pcvl.Processor("SLOS", toffoli)
+    processor.add_port(0, Port(Encoding.DUAL_RAIL, 'ctrl0')) \
+        .add_port(2, Port(Encoding.DUAL_RAIL, 'ctrl1')) \
+        .add_port(4, Port(Encoding.DUAL_RAIL, 'data')) \
+        .add_herald(6, 0) \
+        .add_herald(7, 0) \
+        .add_herald(8, 0) \
+        .add_herald(9, 0) \
+        .add_herald(10, 0) \
+        .add_herald(11, 0)
+    processor.set_postselection(pcvl.PostSelect("[0,1]==1 & [2,3]==1 & [4,5]==1"))
+
+    state_dict = {get_basic_state_from_ports(processor._out_ports, state): str(
+        state) for state in generate_all_logical_states(3)}
+    ca = Analyzer(processor, input_states=state_dict)
+    ca.compute(expected={"000": "000", "001": "001", "010": "010", "011": "011",
+               "100": "100", "101": "101", "110": "111", "111": "110"})
+    assert ca.fidelity == 1
