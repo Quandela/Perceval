604641fd29efa827c1a58360881585ef47aff0f7
Marion Fabre <marion.fabre@quandela.com>
2023-09-05 17:02:50 +0200

    WIP

--- tests/test_3qbits_gates.py
+++ tests/test_3qbits_gates.py
@@ -48,22 +48,59 @@ def test_toffoli_gate_fidelity():
 
 @pytest.mark.skip(reason="Work in progress")
 def test_toffoli_phase():
-    processor = pcvl.Processor("SLOS", 6)
-    processor.add([2, 3], PERM([1, 0]))
-    processor.add([4, 5], PERM([1, 0]))
-    processor.add([0, 1], BS.H())
-    processor.add([2, 3], BS.H())
-    processor.add([4, 5], BS.H())
-    processor.add([0, 1, 2, 3, 4, 5], catalog["toffoli"].build_processor())
-    processor.add([0, 1], BS.H())
-    processor.add([2, 3], BS.H())
-    processor.add([4, 5], BS.H())
+    """Heralding ctrl one after the other to do the same test as test_inverse_cnot_with_H
+    in tests/test_2qbits_gates.py
+    """
+    cnot01 = pcvl.Processor("SLOS", 12)
+    toffoli_port_mapping = [4,5,2,3,0,1]
+    # toffoli_port_mapping = [5,4,3,2,1,0]
+    toffoli_port_mapping.extend([i for i in range(6,12)])
+    cnot01.add([0, 1], BS.H()) \
+        .add([4, 5], BS.H()) \
+        .add(toffoli_port_mapping, catalog["toffoli"].build_circuit()) \
+        .add([0, 1], BS.H()) \
+        .add([4, 5], BS.H()) \
+        .add_port(0, Port(Encoding.DUAL_RAIL, 'ctrl')) \
+        .add_herald(2,0) \
+        .add_herald(3,0) \
+        .add_port(4, Port(Encoding.DUAL_RAIL, 'data')) \
+        .add_herald(6, 0) \
+        .add_herald(7, 0) \
+        .add_herald(8, 0) \
+        .add_herald(9, 0) \
+        .add_herald(10, 0) \
+        .add_herald(11, 0)
+    cnot01.set_postselection(pcvl.PostSelect("[0,1]==1 & [4,5]==1"))
 
-    processor.with_input(BasicState([1, 0, 1, 0, 1, 0]))
-    sampler = Sampler(processor)
-    nb_sample = 10
-    samples = sampler.samples(nb_sample)
-    assert samples['results'].count(BasicState([0, 1, 0, 1, 0, 1])) == nb_sample
+    state_dict = {get_basic_state_from_ports(cnot01._out_ports, state): str(
+        state) for state in generate_all_logical_states(2)}
+    a_cnot01 = Analyzer(cnot01, input_states=state_dict)
+    a_cnot01.compute()
+    pcvl.pdisplay(a_cnot01)
+    # a_cnot01.compute(expected={"00": "00", "01": "01", "10": "11", "11": "10"})
+    # assert a_cnot01.fidelity == 1
+
+    cnot02 = pcvl.Processor("SLOS", 12)
+    cnot02.add([2, 3], BS.H()) \
+        .add([4, 5], BS.H()) \
+        .add(toffoli_port_mapping, catalog["toffoli"].build_circuit()) \
+        .add([2, 3], BS.H()) \
+        .add([4, 5], BS.H()) \
+        .add_herald(0,0) \
+        .add_herald(1,0) \
+        .add_port(2, Port(Encoding.DUAL_RAIL, 'ctrl')) \
+        .add_port(4, Port(Encoding.DUAL_RAIL, 'data')) \
+        .add_herald(6, 0) \
+        .add_herald(7, 0) \
+        .add_herald(8, 0) \
+        .add_herald(9, 0) \
+        .add_herald(10, 0) \
+        .add_herald(11, 0)
+    cnot02.set_postselection(pcvl.PostSelect("[2,3]==1 & [4,5]==1"))
+    a_cnot02 = Analyzer(cnot02, input_states=state_dict)
+    pcvl.pdisplay(a_cnot02)
+    # a_cnot02.compute(expected={"00": "00", "01": "01", "10": "11", "11": "10"})
+    # assert a_cnot02.fidelity == 1
 
 
 def test_full_toffoli():
