205aca84fffc6093ccfe6d4b24783cdbb12df4ac
Melvin Mathe <melvin.mathe@quandela.com>
2023-10-05 15:50:49 +0200

    PEP8 respected using ctrlaltL

--- tests/test_statevector.py
+++ tests/test_statevector.py
@@ -386,10 +386,10 @@ def test_statevector_arithmetic():
 
 
 def test_max_photon_state_iterator():
-    l=[[0,0,0],
-       [1,0,0], [0,1,0], [0,0,1],
-       [2,0,0], [1,1,0], [1,0,1], [0,2,0], [0,1,1], [0,0,2]
-       ]
+    l = [[0, 0, 0],
+         [1, 0, 0], [0, 1, 0], [0, 0, 1],
+         [2, 0, 0], [1, 1, 0], [1, 0, 1], [0, 2, 0], [0, 1, 1], [0, 0, 2]
+         ]
     i=0
     for bs in pcvl.utils.max_photon_state_iterator(3,2):
         assert bs == pcvl.BasicState(l[i])
