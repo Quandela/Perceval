18c47443230210093da684a27a35098ce914e995
raksha singla <raksha.singla@quandela.com>
2023-10-20 09:13:25 +0200

    fix init file for tomography

--- perceval/algorithm/__init__.py
+++ perceval/algorithm/__init__.py
@@ -29,3 +29,4 @@
 
 from .sampler import Sampler
 from .analyzer import Analyzer
+from .tomography.quantum_process_tomography import ProcessTomography, StateTomography
--- perceval/algorithm/tomography/__init__.py
+++ perceval/algorithm/tomography/__init__.py
@@ -26,6 +26,3 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-
-from .abstract_tomography import ATomography
-from .quantum_process_tomography import QuantumProcessTomography, QuantumStateTomography
