e75db2e2c6f35692bd35dc1f11f0599641440b40
Marion Fabre <marion.fabre@quandela.com>
2023-11-30 15:26:32 +0100

    fix shields

--- setup.py
+++ setup.py
@@ -22,7 +22,10 @@ setuptools.setup(
         "Tracker": "https://github.com/Quandela/Perceval/issues"
     },
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
