b0d87c7e6c0723c581aa3c9ff8c7c16337cc445f
Oleksandr Radomskyi <o.radomskyi@gmail.com>
2024-07-24 13:23:47 +0000

    added ISession constructor kwargs for better compatibility when user has a logic for instantiating different providers.

--- perceval/providers/scaleway/scaleway_session.py
+++ perceval/providers/scaleway/scaleway_session.py
@@ -63,6 +63,7 @@ class Session(ISession):
         max_idle_duration_s: int = 1200,
         max_duration_s: int = 3600,
         url: str = _ENDPOINT_URL,
+        **kwargs
     ) -> None:
 
         self._token = token
