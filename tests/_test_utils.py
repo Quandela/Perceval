91a81b1b8b0268c12b42c7049236548c1c5fb539
Eric Bertasi <eric.bertasi@quandela.com>
2025-02-05 12:02:24 +0100

    Fix retry decorator default values + docstring

--- tests/_test_utils.py
+++ tests/_test_utils.py
@@ -223,24 +223,17 @@ class LogChecker:
         self._expected_log_number = expected_log_number
 
 
-def retry(exception_to_check, tries=4, delay=3, backoff=2, logger=None):
+def retry(exception_to_check: type[Exception], tries: int = 4, delay: float = 0, backoff: float = 1, logger=None):
     """Retry calling the decorated function using an exponential backoff.
 
     http://www.saltycrane.com/blog/2009/11/trying-out-retry-decorator-python/
     original from: http://wiki.python.org/moin/PythonDecoratorLibrary#Retry
 
-    :param ExceptionToCheck: the exception to check. may be a tuple of
-        exceptions to check
-    :type ExceptionToCheck: Exception or tuple
-    :param tries: number of times to try (not retry) before giving up
-    :type tries: int
+    :param exception_to_check: the exception type(s) to check; may be a tuple of types
+    :param tries: number of tries (not retries) before giving up
     :param delay: initial delay between retries in seconds
-    :type delay: int
-    :param backoff: backoff multiplier e.g. value of 2 will double the delay
-        each retry
-    :type backoff: int
+    :param backoff: backoff multiplier e.g. value of 2 will double the delay each retry
     :param logger: logger to use. If None, print
-    :type logger: logging.Logger instance
     """
     def deco_retry(f):
 
