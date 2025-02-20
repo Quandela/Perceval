1730d40a7fec030ae7384ee8a960fee1d19c9a0d
Raksha <37671249+raksharuia@users.noreply.github.com>
2025-02-20 11:36:43 +0100

    PCVL-821 Factor converters code (#538)

--- perceval/converters/abstract_converter.py
+++ perceval/converters/abstract_converter.py
@@ -35,6 +35,8 @@ from perceval.utils.algorithms.optimize import optimize
 from perceval.utils.algorithms.norm import frobenius
 import perceval.components.unitary_components as comp
 from perceval.utils.logging import get_logger
+from perceval.converters.converter_utils import label_cnots_in_gate_sequence
+
 
 def _create_mode_map(c_idx: int, c_data: int) -> dict:
     return {c_idx: 0, c_idx + 1: 1, c_data: 2, c_data + 1: 3}
@@ -77,15 +79,29 @@ class AGateConverter(ABC):
     def count_qubits(self, gate_circuit) -> int:
         pass
 
+    @abstractmethod
+    def _get_gate_sequence(self, gate_circuit) -> list[list]:
+        """
+        Iterates over the gate circuit to return a list of Gate operations in the circuit to convert to LO circuit.
+        The information in each element of the list is  - [gate name, gate qubit positions, gate parameters, gate unitary]
+        Note - Gate parameter or gate unitary can be None if value is not needed.
+        """
+        pass
+
+    @abstractmethod
+    def _get_qubit_names(self, gate_circuit, n_qbits) -> list:
+        """
+        Returns a list of Qubit names for a given 'n_qubits' gate circuit. Use 'Q' as the default name
+         unless the framework provides a custom name.
+        """
+        pass
+
     def _configure_processor(self, gate_circuit, **kwargs):
         """
         Sets port Encoding and default input state for the Processor
         """
-        qname = kwargs.get("qname", "Q")  # Default value, set any name provided by the gate circuit
         n_qbits = self.count_qubits(gate_circuit)
-
-        qubit_names = kwargs.get(
-            "qubit_names", [f'{qname}{i}' for i in range(n_qbits)])
+        qubit_names = self._get_qubit_names(gate_circuit, n_qbits)
 
         n_moi = n_qbits * 2  # In dual rail, number of modes of interest = 2 * number of qbits
         self._input_list = [0] * n_moi
@@ -98,11 +114,49 @@ class AGateConverter(ABC):
         default_input_state = BasicState(self._input_list)
         self._converted_processor.with_input(default_input_state)
 
-    @abstractmethod
     def convert(self, gate_circuit, use_postselection: bool = True) -> Processor:
-        pass
+        """Convert a gate-based quantum circuit into a `Processor`.
 
-    def _create_catalog_1_qubit_gate(self, gate_name, **kwargs):
+        :param gate_circuit: gate-based quantum circuit (Qiskit, Myqlm or CQASM)
+        :param use_postselection: when True (default), uses optimized number of `postprocessed CNOT` and
+            'Heralded CNOT' gates. Otherwise, uses only `heralded CNOT`.
+
+        :return: the converted processor
+        """
+        self._configure_processor(gate_circuit)  # empty processor with ports initialized
+        gate_sequence = self._get_gate_sequence(gate_circuit)
+
+        return self._generate_converted_processor(gate_sequence, use_postselection=use_postselection)
+
+    def _generate_converted_processor(self, gate_sequence: list, use_postselection: bool) -> Processor:
+        # gate_inf -> [gate name, gate qubit pos, gate parameter if any or None, unitary Matrix or None]
+
+        optimized_gate_sequence = label_cnots_in_gate_sequence(gate_sequence)
+
+        for gate_index, gate_inf in enumerate(gate_sequence):
+            gate_name = gate_inf[0]
+
+            if len(gate_inf[1]) == 1:
+                if gate_name in catalog:
+                    gate_param = gate_inf[2]
+                    ins = self._create_catalog_1_qubit_gate(gate_name, param=gate_param)
+                else:
+                    ins = self._create_generic_1_qubit_gate(gate_inf[3])
+                    ins._name = gate_inf[0]
+                self._converted_processor.add(gate_inf[1][0] * 2, ins.copy())
+
+            else:
+                if len(gate_inf[1]) > 2:
+                    raise NotImplementedError("2+ Qubit gates not implemented")
+
+                c_idx = gate_inf[1][0] * 2
+                c_data = gate_inf[1][1] * 2
+                self._create_2_qubit_gates_from_catalog(optimized_gate_sequence[gate_index], c_idx, c_data,
+                                                        use_postselection)
+        self.apply_input_state()
+        return self._converted_processor
+
+    def _create_catalog_1_qubit_gate(self, gate_name: str, **kwargs) -> Processor:
         param = kwargs.get("param", None)
         if gate_name in ["rx", "ry", "rz"]:
             return catalog[gate_name].build_processor(theta=param)
