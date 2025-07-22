e6a6345735baa41c4a04e30ab0bb95c9d169059f
Eric Bertasi <eric.bertasi@quandela.com>
2025-07-22 14:36:36 +0200

    Depend on exqalibur 0.8.2

--- setup.py
+++ setup.py
@@ -79,7 +79,7 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     packages=package_list,
-    install_requires=['sympy~=1.12', 'numpy>=1.26,<3', 'scipy~=1.13', 'tabulate~=0.9', 'matplotlib<4', 'exqalibur~=0.8.1',
+    install_requires=['sympy~=1.12', 'numpy>=1.26,<3', 'scipy~=1.13', 'tabulate~=0.9', 'matplotlib<4', 'exqalibur~=0.8.2',
                       'multipledispatch<2', 'protobuf>=3.20.3', 'drawsvg>=2.0', 'requests<3',
                       'networkx~=3.1', 'latexcodec<4', 'platformdirs<5', 'tqdm',
                       "typing_extensions; python_version == '3.9'"  # TODO: remove when dropping 3.9
