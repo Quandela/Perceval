21c9c35063a598ef61797ec75af4d7a66da5b237
AurélienBaert <aurelien.baert@quandela.com>
2025-09-26 10:34:36 +0200

    Peer review

--- tests/test_serialization.py
+++ tests/test_serialization.py
@@ -117,8 +117,8 @@ def test_circuit_serialization_backward_compat():
         "0.11": ":PCVL:zip:eJxVjs0KgkAURh/IlYhgAy5uV9GZrGYqB2cZEs1PYaWhvn2ukvl2Bw6Hj3CUFQE0n/ZrBpLDaI4ToLLtGYVTVX2qg7mM+dQxFPAfvTu2ErwXd6WM+q52AmQnUe/Zq4QNDjc7ZonBIoybaNup5vCgthfwpFG+dAsMc8l7HVySq9dFv7vzP8yeC2n6A8+rQV4=",
         #0.12 : Did not change circuit serialization
         #0.13 : Did not change circuit serialization
-        #1.1 : Added new parameter to Detector
-        "1.1": ":PCVL:Detector:CgRQUE5SEAQYAg=="
+        #1.0 : Added new parameter to Detector
+        "1.0": ":PCVL:Detector:CgRQUE5SEAQYAg=="
     }
     for perceval_version, serial_c in serial_circuits.items():
         try:
