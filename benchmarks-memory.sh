b04cc1704e5d0464e1486dcf13d475924a8e9a5c
Benoit Fanchon <benoit.fanchon@quandela.com>
2026-02-11 18:26:31 +0100

    fix

--- benchmarks-memory.sh
+++ benchmarks-memory.sh
@@ -9,20 +9,18 @@ advancedPath="${scriptPath}/.."
 ##                                                                                                ##
 
 modes=""
-min_photons="4"
-max_photons=""
-# memusage=false
+nphotons="4"
 backends="SLOS SLAP SLOS_CPP SLOS_V2 SLOS_V3"
 mask=""
 compute_method=1
 
 # message strings
-USAGESTR="Usage: $0 [--modes/-m] 20 [--nphotons/-n] 10 [--minphotons/-i] 4 [--backends/-b] \"SLOS_V2 SLOS_V3\" [--mask/-k] \"1010**\" [--compute/-c] 2"
+USAGESTR="Usage: $0 [--modes/-m] 20 [--nphotons/-n] 10 [--backends/-b] \"SLOS_V2 SLOS_V3\" [--mask/-k] \"1010**\" [--compute/-c] 2"
 ERRORSTR="Failed to parse options... exiting."
 
 # option strings
 SHORTOPTIONS="m:n:i:b:k:c:"
-LONGOPTIONS="modes:,nphotons:,minphotons:,backends:,maks:,compute:"
+LONGOPTIONS="modes:,nphotons:,minphotons:,backends:,mask:,compute:"
 
 # read the options
 ARGS=$(getopt --options $SHORTOPTIONS --longoptions $LONGOPTIONS --name "$0" -- "$@") || exit 1
@@ -33,9 +31,7 @@ while true ; do
   case "$1" in
     -h | --help ) echo "$USAGESTR" ; exit 0 ;;
     -m | --modes ) modes="$2" ; shift 2 ;;
-    -n | --nphotons ) max_photons="$2" ; shift 2 ;;
-    -i | --minphoton ) min_photons="$2" ; shift 2 ;;
-    # -u | --memusage ) memusage=true ; shift ;;
+    -n | --nphotons ) nphotons="$2" ; shift 2 ;;
     -b | --backends ) backends="$2" ; shift 2 ;;
     -k | --mask ) mask="$2" ; shift 2 ;;
     -c | --compute ) compute="$2" ; shift 2 ;;
@@ -44,13 +40,7 @@ while true ; do
   esac
 done
 
-# echo ${backends}
-
-# check number of args left
-# [[ $# -ne 1 ]] && echo "$ARGERRORSTR" >&2 && exit 1
-
-# echo scriptPath $scriptPath
 for backend in $backends
 do
-    python ${scriptPath}/Bench_backends.py -u -m ${modes} -n ${max_photons} -i ${min_photons} -b "${backend}" -c ${compute}
+    python ${scriptPath}/Bench_backends.py -u -m ${modes} -n ${nphotons} -b "${backend}" -c ${compute} -k "$mask"
 done
