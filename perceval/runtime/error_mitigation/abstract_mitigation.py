97d430ce0df9124fc43e07ae57cc258761e0db2c
AurélienBaert <aurelien.baert@quandela.com>
2026-06-16 12:34:33 +0200

    Fix imports

--- perceval/runtime/error_mitigation/abstract_mitigation.py
+++ perceval/runtime/error_mitigation/abstract_mitigation.py
@@ -31,10 +31,10 @@ from abc import abstractmethod, ABC
 
 from exqalibur import BSDistribution, BSCount, BSSamples
 
-from utils.constants import KEY_RESULTS, KEY_GLOBAL_PERF, KEY_PHYSICAL_PERF, KEY_LOGICAL_PERF
 from ..computation import Computation
 
 from perceval.utils import NoiseModel, ConversionHelper, apply_min_photons, apply_post_select
+from perceval.utils.constants import KEY_RESULTS, KEY_GLOBAL_PERF, KEY_PHYSICAL_PERF, KEY_LOGICAL_PERF
 from perceval.components import Experiment
 
 
